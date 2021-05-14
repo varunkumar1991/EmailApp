3d9f15292b77143645aa28fbb9d8659d0624ab5d
varunkumar1991 <varunkumar.1991@gmail.com>
2021-05-14 20:15:14 +0530

    mail app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
+import { MatChipList } from '@angular/material/chips';
 import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+import { EmailValidator } from '@angular/forms';
+import { getMatInputUnsupportedTypeError } from '@angular/material/input';
+
+
+// export interface Mail {
+//   name:string;
+// }
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mailApp';
+  public Editor = ClassicEditor;
+  public config = { placeholder: 'Enter here'}
+
+  // Recipient:string="null";
+  // contactemail:any;
+  // visible = true;
+  // selectable = true;
+  // removable = true;
+  // addOnBlur = true;
+  // readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  // mails: Mail[] = [];
+
+  ngOnInit()
+  {
+    console.log("njefbekj");
+  }
+  // add(event: MatChipInputEvent): void {
+  //   const value = (event.value || '').trim();
+
+  //   // Add our fruit
+  //   if (value) {
+  //     this.mails.push({name: value});
+  //   }
+
+
+
+  //   // Clear the input value
+  //   event.chipInput!.clear();
+  // }
+
+  // remove(onemail: Mail): void {
+  //   const index = this.mails.indexOf(onemail);
+
+  //   if (index >= 0) {
+  //     this.mails.splice(index, 1);
+  //   }
+  // }
+
+
+  // public onReady( editor:any ) {
+  //   editor.ui.getEditableElement().parentElement.insertBefore(
+  //       editor.ui.view.toolbar.element,
+  //       editor.ui.getEditableElement()
+  //   );
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import {MatChipInput, MatChipList, MatChipsModule} from '@angular/material/chips';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CKEditorModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
+  exports:[
+    MatFormFieldModule,
+    MatChipsModule,
+    MatChipInput,
+    MatChipList,
+    MatFormField,
+    MatLabel
   ],
   providers: [],
   bootstrap: [AppComponent]
